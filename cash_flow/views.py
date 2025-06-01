2af54c01d767506d6558735ba7e306345d2de4c4
Alexander_Baranov <Sti_ll@mail.ru>
2025-06-01 17:07:14 +0300

    add views.py

--- cash_flow/views.py
+++ cash_flow/views.py
@@ -1,3 +1,138 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from cash_flow.forms import CashFlowForm, StatusForm, TypeForm, CategoryForm, SubCategoryForm
+from cash_flow.models import CashFlow, Status, Type, Category, SubCategory
+from cash_flow.services import get_model_and_form_class
+
+
+class MainListView(ListView):
+    model = CashFlow
+    template_name = "cash_flow/main.html"
+    context_object_name = "cash_flows"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["status"] = Status.objects.all()
+        context["type"] = Type.objects.all()
+        context["categories"] = Category.objects.all()
+        context["sub_categories"] = SubCategory.objects.all()
+
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Фильтрация по дате
+        start_date = self.request.GET.get('start_date')
+        end_date = self.request.GET.get('end_date')
+        if start_date and end_date:
+            queryset = queryset.filter(created_at__range=[start_date, end_date])
+
+        # Фильтрация по статусу
+        status = self.request.GET.get('status')
+        if status:
+            queryset = queryset.filter(status__name=status)
+
+        # Фильтрация по типу
+        type_ = self.request.GET.get('type')
+        if type_:
+            queryset = queryset.filter(type__name=type_)
+
+        # Фильтрация по категории
+        category = self.request.GET.get('category')
+        if category:
+            queryset = queryset.filter(category__name=category)
+
+        # Фильтрация по подкатегории
+        subcategory = self.request.GET.get('subcategory')
+        if subcategory:
+            queryset = queryset.filter(category_sub__name=subcategory)
+
+        return sorted(queryset, key=lambda x: x.created_at, reverse=True)  # Сортируем объекты по дате
+
+
+class CashFlowCreateView(CreateView):
+    form_class = CashFlowForm
+    template_name = "cash_flow/create_cash_flow.html"
+    success_url = reverse_lazy("cash_flow:main")
+
+
+class CashFlowUpdateView(UpdateView):
+    model = CashFlow
+    form_class = CashFlowForm
+    template_name = "cash_flow/create_cash_flow.html"
+    success_url = reverse_lazy("cash_flow:main")
+
+
+class CashFlowDeleteView(DeleteView):
+    model = CashFlow
+    template_name = "cash_flow/delete_cash_flow.html"
+    success_url = reverse_lazy("cash_flow:main")
+
+
+class ReferenceBooksListView(ListView):
+    model = Status
+    template_name = "cash_flow/reference_books.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["all_status"] = Status.objects.all()
+        context["all_type"] = Type.objects.all()
+        context["all_category"] = Category.objects.all()
+        context["all_sub_category"] = SubCategory.objects.all()
+
+        return context
+
+
+class ReferenceBooksCreateView(CreateView):
+    template_name = "cash_flow/reference_books_cud.html"
+    model = None
+    form_class = None
+    success_url = reverse_lazy("cash_flow:reference_books")
+
+    def get(self, request, *args, **kwargs):
+        self.model, _ = get_model_and_form_class(f"{kwargs.get('model')}")
+
+        return super().get(request, *args, **kwargs)
+
+    def get_form_class(self):
+        _, form_class = get_model_and_form_class(self.kwargs.get("model"))
+        return form_class
+
+
+class ReferenceBooksUpdateView(UpdateView):
+    template_name = "cash_flow/reference_books_cud.html"
+    model = None
+    form_class = None
+    success_url = reverse_lazy("cash_flow:reference_books")
+
+    def get(self, request, *args, **kwargs):
+        self.model, _ = get_model_and_form_class(f"{kwargs.get('model')}")
+        return super().get(request, *args, **kwargs)
+
+    def get_form_class(self):
+        _, form_class = get_model_and_form_class(self.kwargs.get("model"))
+        return form_class
+
+    def get_queryset(self):
+        model, _ = get_model_and_form_class(self.kwargs.get("model"))
+        return model.objects.all()
+
+
+class ReferenceBooksDeleteView(DeleteView):
+    model = None
+    template_name = "cash_flow/reference_books_delete.html"
+    success_url = reverse_lazy("cash_flow:reference_books")
+
+    def get(self, request, *args, **kwargs):
+        self.model, _ = get_model_and_form_class(f"{kwargs.get('model')}")
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        model, _ = get_model_and_form_class(self.kwargs.get("model"))
+        return model.objects.all()
